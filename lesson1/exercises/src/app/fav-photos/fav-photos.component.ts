2d99229cf50a5f0d735b5586e76a05e26a91ebe2
Sowmya Bhadraradhya <sowmyab2010@gmail.com>
2020-07-27 13:33:54 -0500

    Updated for Part 1: Modify the CSS

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Fun Outdoor Activities!!!';
+  image1 = 'https://i.pinimg.com/originals/9c/a7/bb/9ca7bb5c0a545987f5554ba7754d49ee.jpg';
+  image2 = 'https://westportmoms.com/wp-content/uploads/sites/47/2017/09/fun_outdoor_kids_activities_1-1.jpg';
+  image3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQPVZOWR0EBnKvWvIfe03cXDDHxdt5aL4WGuw&usqp=CAU';
 
   constructor() { }
 
