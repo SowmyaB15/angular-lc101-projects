75e639670e537d97769ed12c8b7ecd03ee8f5b8d
Sowmya Bhadraradhya <sowmyab2010@gmail.com>
2020-07-28 23:18:49 -0500

    Updated for ngFor and ngIf practice.

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   finishedChores = [];
-
-   trophyImage = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
+   chores = ['Call mom'];//['Complete LaunchCode prep work', 'Buy groceries', 'Empty dishwasher', 'clean bathroom'];
+   finishedChores = ['Complete LaunchCode prep work', 'Buy groceries', 'Empty dishwasher', 'clean bathroom'];
+   // prepWork = ['prep work', 'concept check'];
+   trophyImage = 'https://img.pixers.pics/pho_wat(s3:700/FO/53/88/62/97/700_FO53886297_e4fad572cb0f2b9f1499505d28c7fbeb.jpg,700,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,480,650,jpg)/wall-murals-vector-illustration-of-cartoon-trophy.jpg.jpg';
 
    constructor() { }
 
